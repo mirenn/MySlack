9ad15ae6e26f6b40ad617aa349a49ca40226e714
mirenn <gjwt438mj@gmail.com>
2016-12-10 21:38:15 +0900

    ボタン付けた

--- app/src/main/java/jp/kmc/myslack/MainActivity.java
+++ app/src/main/java/jp/kmc/myslack/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+     //ボタンを設定
+        //Button button=(Button)findViewByID(R.id.buttonA);
     }
 }
